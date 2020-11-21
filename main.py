c61db94ea6a8016675638b53652c30607a69fca8
ohnchen <ohmejona@gmail.com>
2020-11-21 17:01:33 +0100

    adding features

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
+
 import time
+import os 
+import random
+import wikipedia
 
-def time_convert(sec):
-    mins = sec // 60
-    sec = sec % 60
-    if sentence == "This sentence must be completed!!":
-        print("Time:  %d.%d" % (mins, sec))
+random_article = wikipedia.random()
+page = wikipedia.page(random_article)
+
+contents = page.content
+sentences = contents.split(". ")
+
+def random_sentence():
+    sentence = random.choice(sentences)
+    return sentence
+
+def touchtyper(sentence_to_type):
+    global page
+    print(f"[WIKI] {page.title}")
+    print("[INPUT] Type the following sentence and confirm by pressing enter!")
+    print("[EXIT] You want to exit? Type: exit")
+    print("[SENTENCE] ", sentence_to_type)
+
+    solution = input("Type here:  ") 
+    stop = time.time()
+    timeelapsed = stop - start
+       
+    if solution == sentence_to_type:
+        print(f"Well done. No mistakes and you finished in \033[1m{round(timeelapsed, 3)}\033[0m seconds.")
+    elif solution == "exit":
+        return
     else:
-        print("Not correct!!")
+        print(f"You made a few mistakes. Try again!")
+    
+    time.sleep(5)
+    program()
+    
+def program(): 
+    global start
+    os.system("clear")
+    sentence = random_sentence()
+    start = time.time()
+    touchtyper(sentence)
 
-input("Press enter to start")
-start_time = time.time()
-
-sentence = input("This sentence must be completed!!\n")
-stop_time = time.time()
-
-time = stop_time - start_time
-time_convert(time)
+if __name__ == "__main__":
+    program()
